08aef00d09c8afcb9014d34654e5339811608aab
Matteo Passeri aka Sparrow <46816937+theSparrow84@users.noreply.github.com>
2023-05-18 10:48:40 +0200

    case tailoring

--- src/main/java/io/bootify/my_app/repos/ReservationRepository.java
+++ src/main/java/io/bootify/my_app/repos/ReservationRepository.java
@@ -1,7 +1,8 @@
 package io.bootify.my_app.repos;
 
-import io.bootify.my_app.domain.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import io.bootify.my_app.model.Reservation;
 
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
--- src/main/java/io/bootify/my_app/repos/UserRepository.java
+++ src/main/java/io/bootify/my_app/repos/UserRepository.java
@@ -1,7 +1,8 @@
 package io.bootify.my_app.repos;
 
-import io.bootify.my_app.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import io.bootify.my_app.model.User;
 
 
 public interface UserRepository extends JpaRepository<User, Long> {
